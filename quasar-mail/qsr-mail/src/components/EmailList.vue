3ae82b561ee2f8a1bafa8829547056ff77628817
MIU-cz <0151300@gmail.com>
2023-09-28 20:28:14 +0200

    feat: add imap component

---
+++ quasar-mail/qsr-mail/src/components/EmailList.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="email_list">
+		<q-list bordered>
+			<q-item v-for="email in emails" :key="email.id" clickable>
+				<q-item-section avatar>
+					<q-avatar color="primary" text-color="white">
+						{{ email.fromAddress }}
+					</q-avatar>
+				</q-item-section>
+
+				<q-item-section>
+					<q-item-label>{{ email.subject }}</q-item-label>
+					<q-item-label caption>{{ email.messageDt }}</q-item-label>
+				</q-item-section>
+
+				<q-item-section>
+					<q-item-label>{{ email.text }}</q-item-label>
+				</q-item-section>
+
+				<q-item-section side top>
+					<q-icon name="star_border" />
+				</q-item-section>
+			</q-item>
+		</q-list>
+	</div>
+</template>
+  
+<script>
+export default {
+	name: "EmailList",
+	props: {
+		emails: {
+			type: Array,
+			default: () => [],
+		},
+	},
+};
+
+
+</script>
+  
+<style scoped>
+.email_list {
+	max-width: 600px;
+	margin: auto;
+}
+</style>
+  
--- quasar-mail/qsr-mail/src/pages/IndexPage.vue
+++ quasar-mail/qsr-mail/src/pages/IndexPage.vue
@@ -1,17 +1,112 @@
+<!-- 
+Data obdržená z API:    
+subject: string;    
+text: string;    
+fromAddress: string;    
+toAddresses: string[];    
+ccAddresses: string[];    
+bccAddresses: string[];    
+direction: EmailDirectionApi;    
+messageDt: Date; 
+
+mytaskdev@gmail.com
+name+2023
+-->
+
 <template>
   <q-page class="flex flex-center">
-    <img
-         alt="Quasar logo"
-         src="~assets/quasar-logo-vertical.svg"
-         style="width: 200px; height: 200px">
-    <h2>test</h2>
+    <EmailList :emails="emails" />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import EmailList from "src/components/EmailList.vue";
+import { defineComponent } from "vue";
+
+const imap = require("imap");
+const email = require("emailjs");
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  components: { EmailList },
+  data() {
+    return {
+      emails: [],
+    };
+  },
+
+  methods: {
+    mounted() {
+      const client = new imap({
+        user: "mytaskdev@gmail.com",
+        password: "mytaskdev2023",
+        host: "imap.gmail.com",
+        port: 993,
+        tls: true,
+      });
+
+      client.connect();
+
+      client.on("ready", () => {
+        client.openBox("INBOX", false, (err, box) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          client.search(["ALL"], (err, results) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+
+            const stream = client.fetch(results, {
+              bodies: "",
+              struct: false,
+            });
+
+            stream.on("message", (msg, seqno) => {
+              const email = {};
+              msg.on("body", (stream, info) => {
+                let body = "";
+
+                stream.on("data", (chunk) => {
+                  body += chunk.toString("utf8");
+                });
+
+                stream.on("end", () => {
+                  const parsed = email.message(body);
+                  const headers = parsed.headers;
+
+                  email.fromAddress = headers.from;
+                  email.subject = headers.subject;
+                  email.messageDt = headers.date;
+                  email.text = parsed.text;
+                });
+              });
+
+              msg.on("end", () => {
+                this.emails.push(email);
+              });
+            });
+
+            stream.on("end", () => {
+              client.end();
+            });
+          });
+        });
+      });
+
+      client.on("error", (err) => {
+        console.error(err);
+      });
+
+      client.on("close", () => {
+        console.log("Connection closed");
+      });
+    },
+  },
+});
+
+
 </script>
