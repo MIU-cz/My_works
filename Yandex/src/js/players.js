aff0c2b96eb6b95659a82604fbd94c34b011e775
miu-cz <0151300@gmail.com>
2024-06-27 19:58:05 +0200

    feat: add players objects

--- Yandex/src/js/players.js
+++ Yandex/src/js/players.js
@@ -1,1 +1,41 @@
-const playerCards = document.querySelector(".players_cards_wraper");
+const playersCards = document.querySelector(".players_cards_wraper");
+const playerCard = {
+    mainFoto: "src/img/main/img-player.png",
+    mainDescription: "Чемпион мира по шахматам",
+    specDescription: "Гроссмейстер",
+    player: [
+        {id: "hoze", name: "Хозе-Рауль Капабланка"},
+        {id: "emmanuil", name: "Эммануил Ласкер"},
+        {id: "alex", name: "Александр Алехин"},
+        {id: "aron", name: "Арон Нимцович"},
+        {id: "rihard", name: "Рихард Рети"},
+        {id: "ostap", name: "Остап Бендер"} 
+    ]
+};
+
+const addPlayers = () => {
+    let cardsHTML = "",
+        dsc = playerCard.mainDescription;
+
+    for (let player of playerCard.player) {
+        if (player.id == "ostap") {
+            dsc = playerCard.specDescription;
+        }
+        cardsHTML += `
+            <div class="player_card" id="${player.id}">
+                <figure class="card_img">
+                    <img src="${playerCard.mainFoto}" alt="player foto"
+                        onmouseover="this.src='src/img/players/img-player-${player.id}.png'"
+                        onmouseout="this.src='${playerCard.mainFoto}'">
+                </figure>
+                <span class="card_name txt_player_name">${player.name}</span>
+                <description class="card_dsc txt_player_description">${dsc}</description>
+                <a href="#${player.id}" class="card_btn txt_player_btn">Подробнее</a>
+            </div>
+        `;
+    }
+
+    playersCards.innerHTML = cardsHTML;
+};
+
+window.addEventListener("load", addPlayers);
