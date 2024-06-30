9196f6f7a9117401abd10c3033a108d04eb9051d
miu-cz <0151300@gmail.com>
2024-06-30 14:43:09 +0200

    feat: add players slider

--- Yandex/src/js/players.js
+++ Yandex/src/js/players.js
@@ -1,27 +1,27 @@
 const playersCards = document.querySelector(".players_cards_wraper");
 const playerCard = {
-    mainFoto: "src/img/main/img-player.png",
-    mainDescription: "Чемпион мира по шахматам",
-    specDescription: "Гроссмейстер",
-    player: [
-        {id: "hoze", name: "Хозе-Рауль Капабланка"},
-        {id: "emmanuil", name: "Эммануил Ласкер"},
-        {id: "alex", name: "Александр Алехин"},
-        {id: "aron", name: "Арон Нимцович"},
-        {id: "rihard", name: "Рихард Рети"},
-        {id: "ostap", name: "Остап Бендер"} 
-    ]
+	mainFoto: "src/img/main/img-player.png",
+	mainDescription: "Чемпион мира по шахматам",
+	specDescription: "Гроссмейстер",
+	player: [
+		{ id: "hoze", name: "Хозе-Рауль Капабланка" },
+		{ id: "emmanuil", name: "Эммануил Ласкер" },
+		{ id: "alex", name: "Александр Алехин" },
+		{ id: "aron", name: "Арон Нимцович" },
+		{ id: "rihard", name: "Рихард Рети" },
+		{ id: "ostap", name: "Остап Бендер" },
+	],
 };
 
 const addPlayers = () => {
-    let cardsHTML = "",
-        dsc = playerCard.mainDescription;
+	let cardsHTML = "",
+		dsc = playerCard.mainDescription;
 
-    for (let player of playerCard.player) {
-        if (player.id == "ostap") {
-            dsc = playerCard.specDescription;
-        }
-        cardsHTML += `
+	for (let player of playerCard.player) {
+		if (player.id == "ostap") {
+			dsc = playerCard.specDescription;
+		}
+		cardsHTML += `
             <div class="player_card" id="${player.id}">
                 <figure class="card_img">
                     <img src="${playerCard.mainFoto}" alt="player foto"
@@ -33,10 +33,76 @@ const addPlayers = () => {
                 <a href="#${player.id}" class="card_btn txt_player_btn">Подробнее</a>
             </div>
         `;
-    }
+	}
 
-    playersCards.innerHTML = cardsHTML;
-
+	playersCards.innerHTML = cardsHTML;
 };
 
-window.addEventListener("load", addPlayers);
+// window.addEventListener("load", addPlayers);
+addPlayers();
+
+// ============================
+// ### Slider
+const card = document.querySelector(".player_card"),
+	cards = document.querySelectorAll(".player_card"),
+	playersCol = cards.length,
+	playerPrev = document.getElementById("playerPrev"),
+	playerNext = document.getElementById("playerNext"),
+	cardNum = document.querySelector(".card_num"),
+	cardNumOf = document.querySelector(".txt_player_pag-from");
+
+let playersOnPage = 0,
+	playerItem = 0;
+
+function updatePlayers() {
+	playersOnPage = Math.round(playersCards.clientWidth / card.clientWidth);
+	// playerItem = playersOnPage;
+	cardNum.textContent = playersOnPage + playerItem;
+	cardNumOf.textContent = " / " + playersCol;
+
+    // console.log(playerItem);
+	// console.log(card.clientWidth);
+}
+
+function PlayersMove() {
+	playersCards.style.transform = `translateX(${-1 * (card.clientWidth * playerItem)}px)`;
+    cardNum.textContent = playersOnPage + playerItem;
+
+	// console.log(playerItem);
+	// console.log(card.clientWidth);
+}
+
+function PlayerNext() {
+	playerItem += playersOnPage;
+	if (playerItem >= playersCol) {
+		playerItem = 0;
+	}
+	PlayersMove();
+}
+
+function PlayerPrev() {
+	playerItem -= playersOnPage;
+	if (playerItem < 0) {
+		playerItem = playersCol - playersOnPage;
+	}
+	PlayersMove();
+}
+
+playerNext.addEventListener("click", PlayerNext);
+playerPrev.addEventListener("click", PlayerPrev);
+
+window.addEventListener("load", updatePlayers);
+window.addEventListener("resize", function () {
+    updatePlayers();
+    PlayersMove();
+});
+
+// ===> debug
+// console.log(playersCards);
+// console.log(playersCards.clientWidth);
+// console.log(playersCards.scrollWidth);
+// console.log(card);
+// console.log(card.clientWidth);
+// console.log(cards);
+// console.log(playersCol);
+// console.log("- - -");
